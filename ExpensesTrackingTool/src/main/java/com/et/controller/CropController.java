03b14243ff9359ddbdf0f7c205c9069f08053dc9
Srinivas.Deshpande <Srinivas.Deshpande@TES-SrinivasD.as.domcorp.zetainteractive.com>
2018-10-22 22:17:02 +0530

    a

---
+++ ExpensesTrackingTool/src/main/java/com/et/controller/CropController.java
@@ -0,0 +1,39 @@
+package com.et.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.et.entity.CropInformation;
+import com.et.service.CropInformationService;
+
+@RestController
+@RequestMapping("/Crop")
+public class CropController {
+	
+	@Autowired
+	CropInformationService cropDao;
+
+	@RequestMapping(value="/getAllCropInfo",method=RequestMethod.GET)
+	public ResponseEntity<?> getAllCropInformation(){
+		return new ResponseEntity<>(cropDao.getAllCropInformation(),HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/getCropInfoById",method=RequestMethod.POST)
+	public ResponseEntity<?> getCropInfoById(@RequestBody CropInformation cropInfo){
+		return new ResponseEntity<>(cropDao.getCropInfoById(cropInfo.getCropId()),HttpStatus.OK);
+	}
+	
+	@RequestMapping(value="/insertCropInfo",method=RequestMethod.POST)
+	public ResponseEntity<?> insertCropsInfo(@RequestBody CropInformation cropinfo) {
+		if(cropinfo != null && cropinfo instanceof CropInformation)
+		return new ResponseEntity<>(cropDao.insertCropInfo(cropinfo),HttpStatus.OK);
+		else {
+			return new ResponseEntity<>("Provide JSON body",HttpStatus.BAD_REQUEST);
+		}
+	}
+}
