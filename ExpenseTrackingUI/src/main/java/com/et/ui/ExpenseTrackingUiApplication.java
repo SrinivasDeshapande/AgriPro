9e7ead8cc49681ad515f6abbf564c5746a084808
Srinivas.Deshpande <Srinivas.Deshpande@TES-SrinivasD.as.domcorp.zetainteractive.com>
2018-10-25 18:56:47 +0530

    a

---
+++ ExpenseTrackingUI/src/main/java/com/et/ui/ExpenseTrackingUiApplication.java
@@ -0,0 +1,12 @@
+package com.et.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackingUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackingUiApplication.class, args);
+	}
+}
